d8d8c230d2b423039a828bf0dcab0f51b55aa33e
GrinAlexander <elirhard@mail.ru>
2019-11-06 22:09:55 +0300

    Added project
    Relized connection to a database and output of tables

---
+++ CourseProject/CourseProject/Connector.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Controls;
+using System.Data;
+using System.Collections.Generic;
+
+namespace CourseProject
+{
+    //Data Source=.\SQLEXPRESS;Initial Catalog=ERBook;Integrated Security=True
+    class Connector
+    {
+        private string connectionString;
+
+        public Connector()
+        {
+            this.connectionString = CurrentDB.ConnectionString;
+        }
+
+        public async Task<bool> CheckConnectionAsync()
+        {
+            bool result = false;
+            await Task.Run(() =>
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(this.connectionString))
+                {
+                    try
+                    {
+                        sqlConnection.OpenAsync();
+                        sqlConnection.Close();
+                        result = true;
+                    }
+                    catch
+                    {
+                        result = false;
+                    }   
+                }
+            }
+            );
+            return result;
+        }
+
+        public DataTable GetDataTable(string tN)
+        {
+            string tableName = tN;
+            DataTable datatable = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(this.connectionString))
+            {
+                try
+                {
+                    sqlConnection.OpenAsync();
+                    string query = $"SELECT * FROM {tableName}";
+                    SqlCommand command = new SqlCommand(query, sqlConnection);
+                    command.ExecuteNonQuery();
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                    datatable = new DataTable();
+                    sqlDataAdapter.Fill(datatable);
+                    DataTable t = sqlConnection.GetSchema("Tables");
+                    sqlConnection.Close();
+                    return datatable;
+                }
+                catch
+                {
+                    return datatable;
+                }
+            }
+        }
+
+        public List<string> GetListTables()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(this.connectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    List<string> tables = new List<string>();
+                    DataTable dt = sqlConnection.GetSchema("Tables");
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string tablename = (string)row[2];
+                        tables.Add(tablename);
+                    }
+                    sqlConnection.Close();
+                    return tables;
+                }
+                catch
+                {
+                    return new List<string>();
+                }
+            }          
+        }
+    }
+}
---
+++ CourseProject/CourseProject/Tables.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourseProject
+{
+    /// <summary>
+    /// Логика взаимодействия для Tables.xaml
+    /// </summary>
+    public partial class Tables : Window
+    {
+        public Tables()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillComboBox();
+            comboBoxMain.SelectedIndex = 0;
+        }
+
+        private void FillComboBox()
+        {
+            Connector connector = new Connector();
+            List<string> tables = connector.GetListTables();
+            this.comboBoxMain.ItemsSource = tables;
+        }
+
+        private void FillDataGridByTableName(string tableName)
+        {
+            Connector connector = new Connector();
+            this.dataGridMain.ItemsSource = connector.GetDataTable(tableName).DefaultView;
+            labelTableName.Content = $"Выбрана таблица \"{tableName}\":";
+        }
+
+        private void comboBoxMain_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FillDataGridByTableName((sender as ComboBox).SelectedItem as string);
+        }
+    }
+}
