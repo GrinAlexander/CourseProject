6b97a7e8e21897de251bdc85c89e96b9c9d6293b
GrinAlexander <elirhard@mail.ru>
2019-11-09 23:19:03 +0300

    Removed to another rep

--- CourseProject/CourseProject/CurrentDB.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CourseProject
-{
-    public static class CurrentDB
-    {
-        public static string ConnectionString { get; set; } = "Data Source=.\\SQLEXPRESS;Initial Catalog=ERBook;Integrated Security=True";
-    }
-}
